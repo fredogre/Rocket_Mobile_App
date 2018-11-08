e7d773d1c3316cc294b23dc59614627596960288
YOUR NAME <YOUR@EMAIL.com>
2018-11-08 16:05:44 -0500

    Initial Commit

---
+++ Ratings/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ratings
+//
+//  Created by Frederique Grenier on 2018-11-08.
+//  Copyright © 2018 Frederique Grenier. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
