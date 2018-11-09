dcda140d72ec1480b43de41099b5df8a4ee034e8
YOUR NAME <YOUR@EMAIL.com>
2018-11-09 16:23:08 -0500

    first commit

---
+++ Ratings/Elevator.swift
@@ -0,0 +1,9 @@
+//
+//  Elevator.swift
+//  Ratings
+//
+//  Created by Frederique Grenier on 2018-11-08.
+//  Copyright © 2018 Frederique Grenier. All rights reserved.
+//
+
+import Foundation
---
+++ Ratings/SampleData.swift
@@ -0,0 +1,8 @@
+//
+//  SampleData.swift
+//  Ratings
+//
+//  Created by Frederique Grenier on 2018-11-08.
+//  Copyright © 2018 Frederique Grenier. All rights reserved.
+//
+
